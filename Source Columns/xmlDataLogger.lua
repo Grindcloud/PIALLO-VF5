0a04c67d766f2af3032c976e5a30373ddefebef4
Grindcloud Corp <grindcloudcorp@gmail.com>
2026-04-21 18:11:13 +0000

    SOURCE LUA COLUMNS, TRIPROV COMPILER, SWIFT FILES AND ALL THE XML DATA FILES HAVE BEEN ADDED WITH SOME NEW ADDITIONAL FILES.

---
+++ Source Columns/xmlDataLogger.lua
@@ -0,0 +1,13 @@
+local function 
+    when askedForFunction 
+    setParent = 1.2.0.1 (readDown.apply.FORCE::)
+    <<then>: askedForDevice.usual = true.force 
+        when setName.done.FORCE = true.force goDown 
+        then setName (ID.code
+        <<>>) versionID = Version ID 
+        versiondID = VF5.0.0 (VF6.0.0 when released) 
+        then endInstant.end)
+        then  endInstant.endInstant 
+        end
+    end)
+}
