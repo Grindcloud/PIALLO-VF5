9691811bab6d3fa7b2a5b284392e24c972d4cb7e
Grindcloud Corp <grindcloudcorp@gmail.com>
2026-04-21 17:56:14 +0000

    All the existing Rug files also new additional Rug files have been added

---
+++ Rugs/pialloForGaming.rs
@@ -0,0 +1,110 @@
+local function
+when askedForParent = newFunction 
+local function = getParent.gaming.main 
+mainDevice = drive.DirectConnect.mainUser = true 
+setTrue = true 
+    when askedForFunction = runExeGaming.tryOpen.improveHardware 
+    find localServices = DLSS 
+    find localServices = FSR
+    find localServices = DirectX12
+    find localServices = Vulkan
+    find localServices = RayTracing
+    find localServices = DLSS3
+    find localServices = FSR2
+    find localServices = DirectStorage
+    find localServices = MeshShading
+    find localServices = VariableRateShading
+    find localServices = HDR
+    find localServices = AmbientOcclusion
+    find localServices = AntiAliasing
+    find localServices = MotionBlur
+    find localServices = TemporalAA
+    find localServices = ScreenSpaceReflections
+    find localServices = VolumetricLighting
+    find localServices = GlobalIllumination
+    find localServices = SubsurfaceScattering
+    find localServices = ContactShadows
+    find localServices = DepthOfField
+    find localServices = LensFlare
+    find localServices = ChromaticAberration
+    find localServices = FilmGrain
+    find localServices = ColorGrading
+    find localServices = ScreenSpaceShadows
+    find localServices = HighDynamicRange
+    find localServices = LowLevelAPI
+    find localServices = GameWorks
+    find localServices = PhysX
+    find localServices = HairWorks
+    find localServices = Flex
+    find localServices = Flow
+    find localServices = WaveWorks
+    find localServices = Ansel
+    find localServices = ShadowWorks
+    find localServices = GameReadyDrivers
+    find localServices = GeForceExperience
+    find localServices = NVIDIAControlPanel
+    find localServices = NVIDIABroadcast
+    find localServices = NVIDIAReflex
+    find localServices = NVIDIAImageScaling
+    find localServices = NVIDIAEncoder
+    find localServices = NVIDIADecoder
+    find localServices = NVIDIAControl
+    find localServices = NVIDIAPhysX
+    find localServices = NVIDIAOptiX
+    find localServices = NVIDIADeepLearning
+    find localServices = NVIDIADeepLearningSuperSampling
+
+    when functions = runProper.runExeGaming = true 
+    (setTrue = true) goOutBracket 
+    then askForGame = supportGame
+    then connect.mainDevice.mainUser.computer = DirectConnect.Steam = setTrue.force alwaysSetTrue = true 
+    then connect.mainDevice.mainUser.computer = DirectConnect.EpicGames = setTrue.forceAlwaysTrue = true 
+    then connect.mainDevice.mainUser.computer = DirectConnect.UPlay = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.BattleNet = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.RockstarGames = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GOG = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Origin = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.WindowsStore = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.XboxGamePass = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.AmazonLuna = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Stadia = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GeforceNow = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.PlayStationNow = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.NVIDIAShield = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusLink = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.VRChat = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.SteamVR = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.VivePort = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusStore = setTrue.forceAlwaysTrue  = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.HPReverbG2 = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.WMR = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Pimax = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.Index = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.ViveCosmos = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestLink = setTrue.forceAlwaysTrue = true 
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestAirLink = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.OculusQuestVirtualDesktop = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.SteamLink = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.LunaLink = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.XboxCloudGaming = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.PlayStationRemotePlay = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.NintendoSwitchOnline = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GoogleStadia = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.AmazonLuna = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.XboxGameStreaming = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.PlayStationNow = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.NVIDIAGeForceNow = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GoogleStadia = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.AmazonLuna = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.XboxGamePassUltimate = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.PlayStationPlus = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.NintendoSwitchOnline = setTrue.forceAlwaysTrue = true
+    then connect.mainDevice.mainUser.computer = DirectConnect.GoogleStadia = setTrue.forceAlwaysTrue = true
+
+     when GoUp = CheckUp (ReadUp) = force = true 
+     then let 
+     static gaming = true 
+    let decrease = force = true (Allow)
+    then endInstant.end 
+    end)
+}
---
+++ Rugs/reduceMs-PingHandlerWithErrors.rs
@@ -0,0 +1,23 @@
+local module 
+set true after entered STATE = true 
+when true after entered ABS if 
+true in log.Printed (in) 
+out BRACKETS.log.Printed (out) 
+then
+else if functions (FuncTrue) (local(Funcs))
+then set val 
+FuncTrue to true 
+else set val FuncTrue to false 
+        end if 
+    Set "val" to 
+    if FuncTrue then (ACCEPT) else (REJECT)
+end if entered STATE (function FuncTrue) (local(Funcs)2)
+if local FUNCS (FuncTrue) then
+                    same (funcs) 
+                if state entered
+        then set val FuncTrue to true 
+        else set val FuncTrue to false 
+                end if 
+            Set "val" to 
+            if FuncTrue then (ACCEPT) else (REJECT)
+}
